8490e79b063b6b8b587c66ff45bd3345bc6f30ec
ktjhan <tjhan.kavin@gmail.com>
2019-12-16 17:05:23 -0800

    finished

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,64 @@
 // Test away!
+import React from 'react';
+import {render, fireEvent} from '@testing-library/react';
+
+import Controls from './Controls';
+
+test('cannot be closed or opened if locked', () => {
+  const toggle = jest.fn();
+  const { getByText } = render(<Controls locked={true} closed={true} toggle={toggle} />);
+  const openGate = getByText(/open gate/i);
+  fireEvent.click(openGate);
+  expect(toggle).not.toHaveBeenCalled();
+});
+
+
+test('provide buttons to toggle the `closed` and `locked` states', () => {
+  const { getAllByText } = render(<Controls />);
+  const buttons = getAllByText(/gate/i);
+  expect(buttons).toBeDefined();
+});
+
+
+test('buttons text changes to reflect the state the door will be in if clicked', () => {
+  let mockState = {
+    locked: false,
+    closed: false
+  }
+  const toggleClosed = jest.fn();
+  const toggleLocked = jest.fn();
+  const { getAllByText } = render(<Controls
+    locked= {mockState.locked}
+    toggleLocked= {toggleLocked}
+    closed={mockState.closed}
+    toggleClosed= {toggleClosed}
+    />);
+  const [toggleLock, toggleClose] = getAllByText(/gate/i);
+  fireEvent.click(toggleClose);
+  expect(toggleClosed).toHaveBeenCalled();
+  expect(toggleClose.textContent).toBe("Close Gate");
+  expect(toggleLock.textContent).toBe("Lock Gate");
+});
+
+
+test('the closed toggle button is disabled if the gate is locked', () => {
+  const toggleClosed = jest.fn();
+  const { getByText } = render(<Controls
+    locked= {true}
+    toggleClosed= {toggleClosed}
+    />);
+  const closedButton = getByText(/close gate/i);
+  fireEvent.click(closedButton);
+  expect(toggleClosed).not.toHaveBeenCalled();
+});
+
+test('the locked toggle button is disabled if the gate is open', () => {
+  const toggleLocked = jest.fn();
+  const { getByText } = render(<Controls
+    closed = {false}
+    toggleLocked = {toggleLocked}
+    />);
+  const lockedButton = getByText(/lock gate/i);
+  fireEvent.click(lockedButton);
+  expect(toggleLocked).not.toHaveBeenCalled();
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+import React from 'react';
+import {render} from '@testing-library/react';
+
+import Dashboard from './Dashboard';
+
+test('Dashboard shows all', () => {
+  const { getByText } = render(<Dashboard />);
+  getByText(/Unlocked/i);
+  const display = getByText(/Open/i);
+  getByText(/Lock Gate/i);
+  const controls = getByText(/Close Gate/i);
+
+  expect(display).toBeDefined;
+  expect(controls).toBeDefined;  
+});
